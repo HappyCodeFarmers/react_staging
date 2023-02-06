de138275fdf3abfd832c1e2953a9990772bfc025
ljh981102 <haohaos_123@163.com>
2023-02-06 16:29:54 +0800

    测试提交

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h2>hello react</h2>
+        <h2>hello</h2>
       </div>
     )
   }
