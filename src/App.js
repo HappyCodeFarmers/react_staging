f131d0fabed6db843e88f6fcc4be7b77ae1a6400
ljh981102 <haohaos_123@163.com>
2023-02-08 18:36:34 +0800

    redux 学习准备

--- src/App.js
+++ src/App.js
@@ -1,90 +1,11 @@
 import React, { Component } from 'react'
-import { Space, Table, Tag, Button } from 'antd';
 
 export default class App extends Component {
 
-    onChange = (date, dateString) => {
-        console.log(date, dateString);
-    };
-
     render() {
-        const columns = [
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-                render: (text) => <a>{text}</a>,
-            },
-            {
-                title: 'Age',
-                dataIndex: 'age',
-                key: 'age',
-            },
-            {
-                title: 'Address',
-                dataIndex: 'address',
-                key: 'address',
-            },
-            {
-                title: 'Tags',
-                key: 'tags',
-                dataIndex: 'tags',
-                render: (_, { tags }) => (
-                    <>
-                        {
-                        tags.map((tag) => {
-                            let color = tag.length > 5 ? 'geekblue' : 'green';
-                            if (tag === 'loser') {
-                                color = 'volcano';
-                            }
-                            return (
-                                <Tag color={color} key={tag}>
-                                    {tag.toUpperCase()}
-                                </Tag>
-                            );
-                        })}
-                    </>
-                ),
-            },
-            {
-                title: 'Action',
-                key: 'action',
-                render: (_, record) => (
-                    <Space size="middle">
-                        <a>Invite {record.name}</a>
-                        <a>Delete</a>
-                    </Space>
-                ),
-            },
-        ];
-        const data = [
-            {
-                key: '1',
-                name: 'John Brown',
-                age: 32,
-                address: 'New York No. 1 Lake Park',
-                tags: ['nice', 'developer'],
-            },
-            {
-                key: '2',
-                name: 'Jim Green',
-                age: 42,
-                address: 'London No. 1 Lake Park',
-                tags: ['loser'],
-            },
-            {
-                key: '3',
-                name: 'Joe Black',
-                age: 32,
-                address: 'Sydney No. 1 Lake Park',
-                tags: ['cool', 'teacher'],
-            }
-        ];
-
         return (
             <div style={{ width: '1300px', margin: 'auto' }}>
-                <Button type='primary'>Primary Button</Button>
-                <Table columns={columns} dataSource={data} />
+                App...
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ConfigProvider } from 'antd';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // 全局化配置，主题颜色为绿色
-    <ConfigProvider theme={{
-        token: {
-            colorPrimary: '#00b96b',
-        },
-    }}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </ConfigProvider>
 );
