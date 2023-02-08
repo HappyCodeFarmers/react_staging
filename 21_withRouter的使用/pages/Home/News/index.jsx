e0f41baba3164dc1a8dd7e2016ad9592d32243f0
ljh981102 <haohaos_123@163.com>
2023-02-08 14:10:48 +0800

    withRouter的使用

---
+++ 21_withRouter的使用/pages/Home/News/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class News extends Component {
+
+    componentDidMount() {
+        //编程式路由导航
+        // setTimeout(() => {
+        //     this.props.history.push("/home/message")
+        // }, 2000)
+    }
+
+    render() {
+        return (
+            <ul>
+                <li>news001</li>
+                <li>news002</li>
+                <li>news003</li>
+            </ul>
+        )
+    }
+}
