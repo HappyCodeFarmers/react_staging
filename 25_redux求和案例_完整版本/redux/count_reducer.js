a70ef3de55b198d4e533ead14a6572b3cda37f81
lijianhao <haohaos_123@163.com>
2023-02-09 18:02:31 +0800

    redux求和案例_完成版本

--- 25_redux求和案例_完整版本/redux/count_reducer.js
+++ 25_redux求和案例_完整版本/redux/count_reducer.js
@@ -2,7 +2,7 @@
  * 该文件用于创建一个为Count组件服务的reducer，reducer本质是一个函数
  * reducer函数接收两个参数，分别为 之前的状态preState，动作对象action
  */
-import { INCREMENT, DECREMENT } from '../redux/constant'
+import { INCREMENT, DECREMENT } from './constant'
 
 export default function countReducer(preState = 0, action) {
     //从action对象中获取type，data
